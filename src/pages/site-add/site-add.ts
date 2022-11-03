eec7b861658d914ce1e9779e78ac5b0c13e9e7b1
aamirkhanAbacus <aamir.khan@Abacusdesk.in>
2022-11-03 15:15:13 +0530

    3 nov 2022 updated codes

--- src/pages/site-add/site-add.ts
+++ src/pages/site-add/site-add.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Camera, CameraOptions } from '@ionic-native/camera';
+import { ActionSheetController, IonicPage, NavController, NavParams } from 'ionic-angular';
 import { DbserviceProvider } from '../../providers/dbservice/dbservice';
 
 /**
@@ -24,12 +25,18 @@ export class SiteAddPage {
   brandData:any=[];
   pcs:any=[];
   engineer:any=[];
-  constructor(public navCtrl: NavController, public navParams: NavParams, public dbService:DbserviceProvider) {
+  contractorList:any=[];
+  data:any={};
+  flag:boolean=true;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, public dbService:DbserviceProvider , public actionSheetController:ActionSheetController , private camera: Camera,) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad SiteAddPage');
+    this.data.document_image='';
     this.getStates();
+    this.getContractorList();
   }
 
 
@@ -79,5 +86,99 @@ export class SiteAddPage {
   getZoneList(city_name){
 
   }
+
+  getContractorList(){
+
+    this.dbService.show_loading();
+    this.dbService.onPostRequestDataFromApi({},'app_karigar/getContractor',this.dbService.rootUrl).subscribe((ress)=>{
+      console.log(ress);
+      this.contractorList=ress['contractorData'];
+      this.dbService.dismiss_loading();
+    },err=>{
+      this.dbService.errToasr();
+      this.dbService.dismiss_loading();
+    })
+
+  }
+
+  onUploadChange(evt: any) {
+    // this.flag=false;
+    // const file = evt.target.files[0];
+
+    // if (file) {
+    //   const reader = new FileReader();
+
+    //   reader.onload = this.handleReaderLoaded.bind(this);
+    //   reader.readAsBinaryString(file);
+    // }
+    let actionsheet = this.actionSheetController.create({
+      title:" Upload File",
+      cssClass: 'cs-actionsheet',
+
+      buttons:[{
+        cssClass: 'sheet-m',
+        text: 'Camera',
+        icon:'camera',
+        handler: () => {
+          console.log("Camera Clicked");
+          this.takeDocPhoto();
+        }
+      },
+      {
+        cssClass: 'sheet-m1',
+        text: 'Gallery',
+        icon:'image',
+        handler: () => {
+          console.log("Gallery Clicked");
+          this.getDocImage();
+        }
+      },
+      {
+        cssClass: 'cs-cancel',
+        text: 'Cancel',
+        role: 'cancel',
+        handler: () => {
+          console.log('Cancel clicked');
+        }
+      }
+    ]
+  });
+  actionsheet.present();
+}
+takeDocPhoto()
+{
+  console.log("i am in camera function");
+  const options: CameraOptions = {
+    quality: 70,
+    destinationType: this.camera.DestinationType.DATA_URL,
+    targetWidth : 500,
+    targetHeight : 400
+  }
+
+  console.log(options);
+  this.camera.getPicture(options).then((imageData) => {
+    this.flag=false;
+    this.data.document_image = 'data:image/jpeg;base64,' + imageData;
+    console.log(this.data.document_image);
+  }, (err) => {
+  });
+}
+getDocImage()
+{
+  const options: CameraOptions = {
+    quality: 70,
+    destinationType: this.camera.DestinationType.DATA_URL,
+    sourceType: this.camera.PictureSourceType.PHOTOLIBRARY,
+    saveToPhotoAlbum:false
+  }
+  console.log(options);
+  this.camera.getPicture(options).then((imageData) => {
+    this.flag=false;
+    this.data.document_image = 'data:image/jpeg;base64,' + imageData;
+    console.log(this.data.document_image);
+  }, (err) => {
+  });
+}
+
 
 }
